25583fd6c45031d6ca445190dc8e130ceae5085f
Josh Walters <1214370+joshwalters@users.noreply.github.com>
2019-08-01 00:58:18 -0700

    Rename of files

--- generate.sh
+++ generate.sh
@@ -1,30 +1,35 @@
 rm -rf output_svg_tiles
 mkdir output_svg_tiles
 
+SQUARE_SIZE=2000
+
 # Setup black square
-inkscape input_svg_tiles/b.svg --export-png=output_svg_tiles/b.png -w2000 -h2000
-convert -background white -alpha remove -alpha off output_svg_tiles/b.png output_svg_tiles/b.bmp
-potrace -s output_svg_tiles/b.bmp
+inkscape input_svg_tiles/x.svg --export-png=output_svg_tiles/x.png -w${SQUARE_SIZE} -h${SQUARE_SIZE}
+convert -background white -alpha remove -alpha off output_svg_tiles/x.png output_svg_tiles/x.bmp
+potrace -s output_svg_tiles/x.bmp
 
 # Setup white square
-cp input_svg_tiles/w.svg output_svg_tiles/w.svg
+cp input_svg_tiles/z.svg output_svg_tiles/z.svg
 
 # Process
-for VAR in "bp" "br" "bn" "bb" "bq" "bk" "wp" "wr" "wn" "wb" "wq" "wk"
+for PIECE in "p" "r" "n" "b" "q" "k" "o" "t" "m" "v" "w" "l"
 do
+	white_square_piece=$(echo "${PIECE}" | tr '[:lower:]' '[:upper:]')
+	black_square_piece=$(echo "${PIECE}" | tr '[:upper:]' '[:lower:]')
+
 	# Piece SVG to PNG
-	inkscape input_svg_tiles/${VAR}.svg --export-png=output_svg_tiles/${VAR}w.png -w2000 -h2000
-	convert output_svg_tiles/${VAR}w.png output_svg_tiles/${VAR}w.bmp
+	inkscape input_svg_tiles/${PIECE}.svg --export-png=output_svg_tiles/${white_square_piece}.png -w${SQUARE_SIZE} -h${SQUARE_SIZE}
+	convert output_svg_tiles/${white_square_piece}.png output_svg_tiles/${white_square_piece}.bmp
 
 	# Create black square pieces
-	convert output_svg_tiles/b.bmp output_svg_tiles/${VAR}w.png -gravity center -compose over -composite output_svg_tiles/${VAR}b.bmp
+	convert output_svg_tiles/x.bmp output_svg_tiles/${white_square_piece}.png -gravity center -compose over -composite output_svg_tiles/${black_square_piece}.bmp
 
 	# Remove alpha from white square pieces
-	convert -background white -alpha remove -alpha off output_svg_tiles/${VAR}w.png output_svg_tiles/${VAR}w.bmp
+	convert -background white -alpha remove -alpha off output_svg_tiles/${white_square_piece}.png output_svg_tiles/${white_square_piece}.bmp
 
 	# Trace the image
-	potrace -s output_svg_tiles/${VAR}w.bmp
-	potrace -s output_svg_tiles/${VAR}b.bmp
+	potrace -s output_svg_tiles/${white_square_piece}.bmp
+	potrace -s output_svg_tiles/${black_square_piece}.bmp
 done
 
 # Minify the SVG
